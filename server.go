b675e95acab448abf4fcbab83635360ae652da23
Octavian Sopron <sopronoctavian@gmail.com>
2023-12-19 23:26:32 +0200

    Initial commit

---
+++ server.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	val, present := os.LookupEnv("PORT")
+
+	if !present {
+		fmt.Println("PORT is not defined. Defaulting to :1323")
+		val = ":1323"
+	}
+
+	e := echo.New()
+
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+
+	e.Logger.Fatal(e.Start(val))
+}
